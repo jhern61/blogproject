8f33c320b71ef0b0c3d1df8b1bb21b91462964ea
Joe <jhern61@gmail.com>
2019-04-04 18:20:02 -0500

    add user class, got mongo to work.

--- src/main/java/BlogClient.java
+++ src/main/java/BlogClient.java
@@ -1,10 +1,38 @@
-import java.util.*;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.Scanner;
 
 public class BlogClient {
 
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+
+
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        //Connect to database.
+        MongoDatabase database = mongoClient.getDatabase("BlogDatabase");
+
+
+        MongoCollection<Document> userCollection = database.getCollection("User");
+        MongoCollection<Document> postCollection = database.getCollection("Post");
+
+        //Create User.
+        Document user = new Document("userName", "Joe")
+                .append("password", "password");
+
+        //Insert object into collection.
+        userCollection.insertOne(user);
+
+
+        // Delete Car
+        postCollection.deleteOne(new Document("model", "iPhone X"));
+
+
 
 
         //Variables
@@ -17,8 +45,12 @@ public class BlogClient {
 
         while (flag) {
             switch (userCommand) {
+                //Login
                 case 1:
+
                     break;
+
+                //Register
                 case 2:
                     break;
                 default:
---
+++ src/main/java/User.java
@@ -0,0 +1,31 @@
+public class User {
+
+    private final String password;
+    private final String username;
+
+    public User(String username, String password) {
+        this.password = password;
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public static User create(String username, UserRepository repository) {
+        return repository.find(username);
+    }
+
+    public boolean authenticate(String password) {
+        return this.password.equals(password);
+    }
+
+    public static User create(String username) {
+        return create(username, new UserRepository());
+    }
+
+}
