c7feb217aaae1b3d98b9cd5bee52c5d8d8a00daa
Joe <jhern61@gmail.com>
2019-04-04 14:19:51 -0500

    2nd commit add base stuff.

--- src/main/java/BlogClient.java
+++ src/main/java/BlogClient.java
@@ -1,2 +1,56 @@
+import java.util.*;
+
 public class BlogClient {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+        //Variables
+        boolean flag = true;
+
+        //Show login menu
+        loginMenu();
+        System.out.print("\nEnter command: ");
+        int userCommand = scanner.nextInt();
+
+        while (flag) {
+            switch (userCommand) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                    default:
+                        System.out.println("invalid command");
+
+            }
+        }
+
+
+
+    }
+
+    public static void loginMenu(){
+        System.out.println("\n----------Welcome----------------\n" +
+                             "\n1 - Login " +
+                            "\n2 - Register");
+
+    }
+
+    public static void login(String userName, String password){
+
+    }
+
+    public static void register(String userName, String password) {
+
+    }
+
+
 }
+
+
+
+
+
+
